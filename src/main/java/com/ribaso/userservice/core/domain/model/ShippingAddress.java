8cd47d4049f83692f9bd8fae6299b55c1b0309ac
Bastian Lehmann <mail@bastian-lehmann.de>
2024-06-19 22:52:59 +0200

    implemented models

--- src/main/java/com/ribaso/userservice/core/domain/model/ShippingAddress.java
+++ src/main/java/com/ribaso/userservice/core/domain/model/ShippingAddress.java
@@ -1,5 +1,27 @@
 package com.ribaso.userservice.core.domain.model;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Embeddable
 public class ShippingAddress {
 
+    private String firstname;
+
+    private String lastname;
+
+    private String street;
+
+    private String houseNumber;
+
+    private String postalCode;
+
+    private String state;
+
+    private String city;
 }
--- src/main/java/com/ribaso/userservice/core/domain/model/User.java
+++ src/main/java/com/ribaso/userservice/core/domain/model/User.java
@@ -1,5 +1,59 @@
 package com.ribaso.userservice.core.domain.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "USER_ID")
+    private UUID id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "firstname", column = @Column(name = "billing_first_name")),
+        @AttributeOverride(name = "lastname", column = @Column(name = "billing_last_name")),
+        @AttributeOverride(name = "street", column = @Column(name = "billing_street")),
+        @AttributeOverride(name = "houseNumber", column = @Column(name = "billing_house_number")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "billing_postal_code")),
+        @AttributeOverride(name = "state", column = @Column(name = "billing_state")),
+        @AttributeOverride(name = "city", column = @Column(name = "billing_city"))
+    })
+    private BillingAddress billingAddress;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "firstname", column = @Column(name = "shipping_first_name")),
+        @AttributeOverride(name = "lastname", column = @Column(name = "shipping_last_name")),
+        @AttributeOverride(name = "street", column = @Column(name = "shipping_street")),
+        @AttributeOverride(name = "houseNumber", column = @Column(name = "shipping_house_number")),
+        @AttributeOverride(name = "postalCode", column = @Column(name = "shipping_postal_code")),
+        @AttributeOverride(name = "state", column = @Column(name = "shipping_state")),
+        @AttributeOverride(name = "city", column = @Column(name = "shipping_city"))
+    })
+    private ShippingAddress shippingAddress;
 }
